fb0b4f34eae5007eccfee2a1575e7f37200247ef
Celestenono <noemie.moreau96@gmail.com>
2024-03-01 17:18:34 +0100

    try cellvit training

--- cell_segmentation/utils/post_proc_cellvit.py
+++ cell_segmentation/utils/post_proc_cellvit.py
@@ -80,13 +80,14 @@ class DetectionCellPostProcessor:
                     For each instance, the dictionary contains the keys: bbox (bounding box), centroid (centroid coordinates),
                     contour, type_prob (probability), type (nuclei type)
         """
+        print(self.nr_types)
         if self.nr_types is not None:
             pred_type = pred_map[..., :1]
             pred_inst = pred_map[..., 1:]
             pred_type = pred_type.astype(np.int32)
         else:
             pred_inst = pred_map
-        print(pred_inst.shape)
+
         pred_inst = np.squeeze(pred_inst)
         pred_inst = self.__proc_np_hv(
             pred_inst, object_size=self.object_size, ksize=self.k_size
