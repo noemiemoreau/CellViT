c861974e90124d6ac2aecde258c9a200c65fdd77
Celestenono <noemie.moreau96@gmail.com>
2025-04-03 10:48:43 +0200

    debug

--- cell_segmentation/datasets/pannuke.py
+++ cell_segmentation/datasets/pannuke.py
@@ -90,7 +90,7 @@ class PanNukeDataset(CellDataset):
                     logger.debug(
                         "Found image {fold_image}, but no corresponding annotation file!"
                     )
-            fold_types = pd.read_csv(self.dataset / f"fold{fold}" / "types.csv")
+            fold_types = pd.read_csv(self.dataset / f"fold{fold}" / "types.csv", sep=";")
             print(fold_types.head())
             print(fold_types.columns)
             fold_type_dict = fold_types.set_index("img")["type"].to_dict()
