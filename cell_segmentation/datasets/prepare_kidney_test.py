08d1de5352ca2f3032316b46b185f0da3e71eb48
Celestenono <noemie.moreau96@gmail.com>
2025-05-08 16:11:18 +0200

    debug

--- cell_segmentation/datasets/prepare_kidney_test.py
+++ cell_segmentation/datasets/prepare_kidney_test.py
@@ -108,14 +108,18 @@ if __name__ == "__main__":
                 print(WSI_roi.shape)
                 print(GT_inst_map.shape)
                 print(GT_type_map.shape)
-                if WSI_roi.shape[0] <= 1000 or WSI_roi.shape[1] <= 1000:
+                if WSI_roi.shape[0] < 1000:
                     diff_x = 1000 - WSI_roi.shape[0]
-                    diff_y = 1000 - WSI_roi.shape[1]
                     pad_x = diff_x // 2
+                    WSI_roi = np.pad(WSI_roi, ((pad_x, pad_x), (0, 0)), mode='constant')
+                    GT_inst_map = np.pad(GT_inst_map, ((pad_x, pad_x), (0, 0)), mode='constant')
+                    GT_type_map = np.pad(GT_type_map, ((pad_x, pad_x), (0, 0)), mode='constant')
+                if WSI_roi.shape[1] < 1000:
+                    diff_y = 1000 - WSI_roi.shape[1]
                     pad_y = diff_y // 2
-                    WSI_roi = np.pad(WSI_roi, ((pad_x, pad_x), (pad_y, pad_y)), mode='constant')
-                    GT_inst_map = np.pad(GT_inst_map, ((pad_x, pad_x), (pad_y, pad_y)), mode='constant')
-                    GT_type_map = np.pad(GT_type_map, ((pad_x, pad_x), (pad_y, pad_y)), mode='constant')
+                    WSI_roi = np.pad(WSI_roi, ((0, 0), (pad_y, pad_y)), mode='constant')
+                    GT_inst_map = np.pad(GT_inst_map, ((0, 0), (pad_y, pad_y)), mode='constant')
+                    GT_type_map = np.pad(GT_type_map, ((0, 0), (pad_y, pad_y)), mode='constant')
                 if WSI_roi.shape[0] > 1000 :
                     diff_x = WSI_roi.shape[0] - 1000
                     start_x = diff_x//2
