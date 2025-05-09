f43ded6c7f2cad73c702e089192f725666dd1a5c
Celestenono <noemie.moreau96@gmail.com>
2025-05-09 14:51:16 +0200

    debug

--- cell_segmentation/inference/inference_cellvit_experiment_kidney.py
+++ cell_segmentation/inference/inference_cellvit_experiment_kidney.py
@@ -1084,7 +1084,8 @@ class MoNuSegInference:
         )
 
         result_dict = {"inst_map": pred_sample_instance_maps, "type_map": pred_sample_type_maps}
-        np.save(save_dir + f"{img_name}" + ".npy",
+        print(img_name)
+        np.save(save_dir / f"{img_name}.npy",
                 result_dict)
 
         gt_sample_binary_map = (
